daaedfa2ab7cb97701f5f208b8e623158588ad87
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-12-12 14:27:14 +0100

    push del proyecto vacio

---
+++ api_monumentos_v2/src/test/java/com/salesianos/triana/api_monumentos_v2/ApiMonumentosV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.api_monumentos_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMonumentosV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
